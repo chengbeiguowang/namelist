dab3bf0fd4a40c853ce59b6219cbb6fc868aec0b
zuoshicheng <zuoshicheng91@163.com>
2024-08-24 01:36:11 +0800

    引入count计数

--- dexnamelist.py
+++ dexnamelist.py
@@ -40,58 +40,63 @@ def scdex(input_namelist, file_base):
     race_dict = read_namelist_to_team(input_namelist)
     table_prefix = '2024年“数据要素×”大赛四川分赛初赛评分表_'
     table_subfix = '.xlsx'
+    count = 0
     for partition in race_array:
         dest_excel = file_base + BACK_SLASH + partition + BACK_SLASH + table_prefix + partition + table_subfix
         dest_dir = file_base + BACK_SLASH + partition
         if not os.path.isdir(dest_dir) or not os.path.exists(dest_excel):
             continue
-        write_to_score_sheet(race_dict, dest_excel, dest_dir)
+        count += write_to_score_sheet(race_dict[partition], dest_excel, dest_dir)
+    print(str(count))
 
 
-def write_to_score_sheet(race_dict, workbook_path, file_base):
+def write_to_score_sheet(partition_dict, workbook_path, file_base) -> int:
     wb = openpyxl.load_workbook(workbook_path)
     sheet = wb['Sheet1']
 
     clean_up(sheet, 5, 10)
 
-    for race_key in race_dict:
-        race_partition_dict = race_dict[race_key]
-        row = 5
-        for key in race_partition_dict:
-            partition_array = race_partition_dict[key]
-            partition_base = file_base + BACK_SLASH + key
-            for i in range(len(partition_array)):
-                team: Team = partition_array[i]
-                if team.work_name is None or team.work_name == '':
-                    continue
+    row = 5
+    count = 0
+    for key in partition_dict:
+        partition_array = partition_dict[key]
+        partition_base = file_base + BACK_SLASH + key
+        for i in range(len(partition_array)):
+            team: Team = partition_array[i]
+            count += 1
+            if team.work_name is None or team.work_name == '':
+                continue
 
-                team_prefix = '【'
-                team_subfix = '】'
-                team_base = partition_base + BACK_SLASH + team_prefix + team.name + team_subfix
-                # program_base = team_base + BACK_SLASH + '项目申报书'
+            team_prefix = '【'
+            team_subfix = '】'
+            team_base = partition_base + BACK_SLASH + team_prefix + team.name + team_subfix
+            # program_base = team_base + BACK_SLASH + '项目申报书'
 
-                ## tmp logic
-                if not os.path.isdir(team_base):
-                    continue
+            ## tmp logic
+            if not os.path.isdir(team_base):
+                print(team_base)
+                continue
 
-                fill_cell(sheet.cell(row, column=1), str(row - 4))
-                fill_cell(sheet.cell(row, column=2), str(team.race_name))
-                fill_cell(sheet.cell(row, column=3), str(team.race_partition))
-                fill_cell(sheet.cell(row, column=4), str(team.name))
-                link_file = ".\\" + key + BACK_SLASH + team_prefix + team.name + team_subfix + BACK_SLASH + get_program_file_name(team_base)
-                fill_cell_link(sheet.cell(row, column=5), str(team.work_name), link_file)
+            fill_cell(sheet.cell(row, column=1), str(row - 4))
+            fill_cell(sheet.cell(row, column=2), str(team.race_name))
+            fill_cell(sheet.cell(row, column=3), str(team.race_partition))
+            fill_cell(sheet.cell(row, column=4), str(team.name))
+            link_file = ".\\" + key + BACK_SLASH + team_prefix + team.name + team_subfix + BACK_SLASH + get_program_file_name(
+                team_base)
+            fill_cell_link(sheet.cell(row, column=5), str(team.work_name), link_file)
 
-                # 累加和
-                cell = sheet.cell(row, column=10)
-                cell.value = "=SUM({}:{})".format("F" + str(row), "I" + str(row))
-                cell.alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
-                font = Font('Times New Roman', color='000000', bold=False, size=FONT_SIZE)
-                cell.font = font
+            # 累加和
+            cell = sheet.cell(row, column=10)
+            cell.value = "=SUM({}:{})".format("F" + str(row), "I" + str(row))
+            cell.alignment = Alignment(horizontal='center', vertical='center', wrapText=True)
+            font = Font('Times New Roman', color='000000', bold=False, size=FONT_SIZE)
+            cell.font = font
 
-                sheet.row_dimensions[row].height = 100
-                row += 1
+            sheet.row_dimensions[row].height = 100
+            row += 1
 
     wb.save(workbook_path)
+    return count
 
 
 def get_program_file_name(work_dir) -> str:
@@ -111,6 +116,7 @@ def read_namelist_to_team(workbook_path) -> dict:
     team_set = set()
     race_dict = {}
 
+    count = 0
     for row in range(2, sheet.max_row):
         # 参赛团队
         team_name = sheet.cell(row, column=3).value
@@ -136,6 +142,7 @@ def read_namelist_to_team(workbook_path) -> dict:
             race_dict[race_name][race_partition] = []
 
         race_dict[race_name][race_partition].append(team)
+        count += 1
 
-    print(str(race_dict))
+    print(str(count))
     return race_dict
